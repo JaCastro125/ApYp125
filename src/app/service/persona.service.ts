e2be468ecbed967d4663e695006f1a7c20722aa8
Javier Castro <yoprogramo125@gmail.com>
2023-04-08 16:35:23 -0300

    corrigiendo
    corrigiendo skills

--- src/app/service/persona.service.ts
+++ src/app/service/persona.service.ts
@@ -1,19 +1,20 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+//import { environment } from 'src/environments/environment';
 import { Persona } from '../model/persona.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PersonaService {
   URL = 'http://localhost:8080/personas/';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
   public getPersona(): Observable<Persona>{
-    return this.httpClient.get<Persona>(this.URL + 'lista');
-    
+    return this.http.get<Persona>(this.URL+ 'traer/perfil');
   }
 }
 
